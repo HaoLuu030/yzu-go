e71193d4249d7139a8998e22ff6f1d585bc05ebf
Hao Luu <luuquachgiahao99@gmail.com>
2025-12-15 10:30:12 +0800

    init db

---
+++ js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSsz8iRV4A2pN0Xv2lfFPscovqONg01Ws",
+  authDomain: "yzu-go.firebaseapp.com",
+  projectId: "yzu-go",
+  storageBucket: "yzu-go.firebasestorage.app",
+  messagingSenderId: "703903780258",
+  appId: "1:703903780258:web:e86ae40f163dc41c3d36fb",
+  measurementId: "G-W7JKJB7865"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
