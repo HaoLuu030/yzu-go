d6e0c69b6736eadea1fe262caf7486ef068fba86
Yenhi <yenhi130406@gmail.com>
2025-12-15 17:11:39 +0800

    field-track

--- logScore.js
+++
@@ -1,9 +0,0 @@
-// Universal save function for all levels
-function saveLevelProgress(levelName, score) {
-    const data = {
-        score: score,
-        unlocked: true
-    };
-
-    localStorage.setItem(levelName, JSON.stringify(data));
-}
