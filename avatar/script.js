f5f966b9db314d9e8a513c1d66459ce41e866130
Hao Luu <luuquachgiahao99@gmail.com>
2025-12-18 18:06:38 +0800

    added loading for avatar

--- avatar/script.js
+++ avatar/script.js
@@ -1,3 +1,31 @@
+import { startLoader } from "../shared/loader/index.js";
+
+
+startLoader({
+    text: "Swiping student ID...",
+    assets: [
+        "image/1.png",
+        "image/10.png",
+        "image/11.png",
+        "image/12.png",
+        "image/13.png",
+        "image/14.png",
+        "image/15.png",
+        "image/16.png",
+        "image/17.png",
+        "image/18.png",
+        "image/19.png",
+        "image/2.png",
+        "image/20.png",
+        "image/3.png",
+        "image/4.png",
+        "image/5.png",
+        "image/6.png",
+        "image/7.png",
+        "image/8.png",
+        "image/9.png",
+    ]
+})
 /* CONFIG */
 const avatars = Array.from({ length: 20 }, (_, i) => ({
     id: i + 1,
@@ -50,7 +78,7 @@ finishBtn.onclick = () => {
     window.location.href = MAP_URL;
 };
 
-// TODO: 
+// TODO:
 // loading page for this page
 // create an ID
 // make a key that stores ID, name and avatar ID
